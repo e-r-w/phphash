dc0d65f1d170d5ff92b4e13c30fc9f832fe2fde6
Matt Murphy <caevyn@gmail.com>
2015-02-10 13:34:26 +1100

    Update index.php

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
    $id = $_GET["id"];
    $ts= $_GET["ts"];
    $hash = $_GET["hash"];
-
-   $myhash = hash_hmac('sha256', "{$id}{$ts}", 'secret');
+   $data = $id . $ts
+   $myhash = hash_hmac('sha256', $data, 'secret');
    if($myhash == $hash){
-     echo "valid, you are that guy I know who has the same secret key as me."
+     echo "valid, you are that guy I know who has the same secret key as me.";
    }else{
-     echo "Hey, you don't even know the password"
+     echo "Hey, you don't even know the password";
    }
    
  ?> 
