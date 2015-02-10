0af8162233342f52ee6308b55717704f244af4a4
Matt Murphy <m.murphy@nib.com.au>
2015-02-10 12:00:25 +1100

    hi

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
  </head>
  <body>
  <?php
-   $qs = $_SERVER['QUERY_STRING']
-
+   
+   $id = $_GET['id']
+   $time = $_GET['time']
+   $qshash = $_GET['hash']
+   $hash = hash_hmac('sha256', "{$id}{$time}", 'secret');
    
    echo $qs;
  ?> 
