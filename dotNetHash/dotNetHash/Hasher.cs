5deaa35098d9a021515f099b4f0a573dc640e8cf
Ethan Williams <E.Williams@nib.com.au>
2015-02-10 16:15:52 +1100

    added .NET hasher

---
+++ dotNetHash/dotNetHash/Hasher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNetHash
+{
+    public static class Hasher
+    {
+        private const string Key = "secret";
+
+        public static string Hash(string id, string timeStamp)
+        {
+            var keyAsBarray = GetBytes(Key);
+            using (var hmac = new HMACSHA256(keyAsBarray))
+            {
+                var data = id + timeStamp;
+                var dataAsBarray = GetBytes(data);
+                
+                hmac.ComputeHash(dataAsBarray);
+                var hashString = GetString(hmac.Hash);
+                return hashString;
+            }
+        }
+
+        private static byte[] GetBytes(string str)
+        {
+            return Encoding.UTF8.GetBytes(str);
+        }
+
+        private static string GetString(byte[] ba)
+        {
+            var hex = BitConverter.ToString(ba);
+            return hex.Replace("-", "").ToLower();
+        }
+    }
+}
---
+++ dotNetHash/dotNetHash/HasherTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace dotNetHash
+{
+    [TestFixture]
+    public class HasherTest
+    {
+        [Test]
+        public void ShouldHashThings()
+        {
+            var expectedHash = "b4a68e00d87c605b4d251617fb7a7e101ef7a0d6329263065493920972794779";
+            var id = "12345";
+            var timeStamp = "2015-02-10T10:22:13";
+            var hash = Hasher.Hash(id, timeStamp);
+            Assert.That(hash, Is.EqualTo(expectedHash));
+        }
+    }
+}
